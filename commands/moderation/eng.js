d8759a8ee8fb212d3f68700a298b1c9c93f88747
Fire <kenxo69official@gmail.com>
2020-12-10 03:52:46 +0600

    Create eng.js

---
+++ commands/moderation/eng.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require("discord.js");
+const { greenlight } = require("../../JSON/colours.json")
+
+module.exports = {
+    config: {
+        name: "eng",
+        category: "moderation",
+        noalias: ['english'],
+        description: "Says user to talk in english",
+        usage: "[text]",
+        accessableby: "everyone"
+    },
+    run: async (bot, message, args) => {
+      try {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return;
+        message.delete({ timeout: 100 })
+
+let tig = args.join(' ') || " ";
+
+        const embed = new MessageEmbed()
+            .setDescription(`Please do not use any language asides of ENGLISH is this server.We may not be able to assist you if you use different language, ${tig}`)
+            .setColor(greenlight);
+
+        message.channel.send(embed)
+      } catch (e) {
+          throw e;
+      };
+  }
+};
